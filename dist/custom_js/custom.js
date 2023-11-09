fb969d4b509292f5ecaf6a4b51babf0aee2a02e4
yooonmi12 <122426957+yooonmi12@users.noreply.github.com>
2023-11-09 17:10:57 +0900

    틀 작업 진행중

--- dist/custom_js/custom.js
+++ dist/custom_js/custom.js
@@ -46,6 +46,9 @@ const leftMenuHandle = () => {
     function toggleSideNav() {
         const sideNav = document.querySelector(".side-nav");
         sideNav.classList.toggle("fold");
+        if(window.innerWidth < 1200 && window.innerWidth > 640){
+            $(".side-nav-bg").fadeToggle(200);
+        }
 
         const content = document.querySelector(".content");
         content.classList.toggle("fold");
@@ -55,7 +58,10 @@ const leftMenuHandle = () => {
     }
 
     const foldBtn = document.querySelector(".fold_btn");
+    const sideBg = document.querySelector(".side-nav-bg");
     foldBtn.addEventListener("click", toggleSideNav);
+    sideBg.addEventListener("click", toggleSideNav);
+
 
     // fold 클래스 체크 함수
     function checkFold() {
@@ -99,13 +105,73 @@ const leftMenuHandle = () => {
 
 }
 
-function topbarHandle(){
-    $(".top-bar .profile_btn").on('mouseenter',function(){
-        $(this).find(".profile_box").addClass('open');
+const topbarHandle = ()=>{
+    // 화폐
+    const walletList = document.querySelectorAll('.wallet_list > div')
+    const walletHandle = (e)=>{
+        const parentNode = e.currentTarget.parentElement.parentElement;
+        parentNode.querySelectorAll('.wallet_list > div').forEach((item)=>{
+            item.classList.remove('active')
+        })
+        e.currentTarget.classList.add('active');
+
+        const img = e.currentTarget.querySelector('img').src
+        const text = e.currentTarget.querySelector('p.text-right strong').innerText
+        const text2 = e.currentTarget.querySelector('p.text-right span').innerText
+
+        const button = document.querySelector(`.${parentNode.dataset.btn}`)
+        button.querySelector('img').src = img
+        button.querySelector('strong').innerText = text
+        button.querySelector('span').innerText = text2
+
+        setTimeout(function () {
+            var el = document.querySelector(".walletDropdown");
+            var dropdown = tailwind.Dropdown.getOrCreateInstance(el);
+            dropdown.hide();
+        }, 10);
+    }
+
+    walletList.forEach((item)=>{
+        item.addEventListener("click",walletHandle)
     })
-    $(".top-bar .profile_btn").on('mouseleave',function(){
-        $(this).find(".profile_box").removeClass('open');
-    })
+
+    // 법정화폐보기 버튼 
+    const legalBtn = document.querySelector('.legal_check');
+    const legalHandle = (e)=>{
+        const walletWrap = document.querySelector('.walletDropdown')
+        const walletBtn = document.querySelector('.wallet_top_btn')
+        if(e.target.checked){
+            walletWrap.classList.remove('nolegal')
+            walletBtn.classList.remove('nolegal')
+        }else{
+            walletWrap.classList.add('nolegal')
+            walletBtn.classList.add('nolegal')
+        }
+    }
+    legalBtn.addEventListener("change",legalHandle)
+
+    // 작게보기 버튼
+    const smallBtn = document.querySelector(".small_check")
+    const smallHandle = (e)=>{
+        const list = document.querySelectorAll('.walletDropdown .wallet_list > div')
+        const tit = document.querySelectorAll('.walletDropdown .wallet_tit')
+        if(e.target.checked){
+            list.forEach((item)=>{
+                if(!item.classList.contains('isItem')){
+                    item.classList.add("hidden")
+                }
+            })
+            tit.forEach((item)=>{ item.classList.add("hidden")})
+        }else{
+            list.forEach((item)=>{
+                if(!item.classList.contains('isItem')){
+                    item.classList.remove("hidden")
+                }
+            })
+            tit.forEach((item)=>{ item.classList.remove("hidden")})
+        }
+    }
+    smallBtn.addEventListener("change",smallHandle)
 }
 
 //=======================================================
@@ -142,3 +208,21 @@ window.addEventListener("load", function () {
     // 3초 후에 hideLoadingSvg() 함수 호출
     setTimeout(hideLoadingSvg, 500); // 3초(3000ms)로 설정
 });
+
+
+//=======================================================
+//   윈도우 resize
+//=======================================================
+const resizeHandle = ()=>{
+    if(window.innerWidth > 1200){
+        $(".side-nav-bg").fadeOut(200);
+    }else{
+        const side = document.querySelector('.side-nav')
+        if(!side.classList.contains('fold')){
+            $(".side-nav-bg").fadeIn(200);
+        }else{
+            $(".side-nav-bg").fadeOut(200);
+        }
+    }
+}
+window.addEventListener("resize",resizeHandle)
