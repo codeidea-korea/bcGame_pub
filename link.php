82aa4bfca1d1ac1f414b8b83d1702ed53a24e8e0
yooonmi12 <122426957+yooonmi12@users.noreply.github.com>
2023-12-21 17:13:47 +0900

    링크 마무리

--- link.php
+++ link.php
@@ -196,6 +196,13 @@ echo txtRecord('./@record/');
 			</ul>
 		</li>
 
+        <li class="mt20" data-label="스포츠">
+            <ul>
+				<li><a href="/bcGame/sports.html" target="_blank" class="">스포츠</a></li>
+				<li><a href="/bcGame/sports_detail.html" target="_blank" class="">스포츠 - 상세 </a></li>
+            </ul>
+        </li>
+        
         <li class="mt20" data-label="레이싱">
             <ul>
 				<li><a href="/bcGame/racing.html" target="_blank" class="">레이싱</a></li>
